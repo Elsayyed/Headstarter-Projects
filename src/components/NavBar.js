705799c3ece956432861db0283f1c6ea7b80acee
unknown <upmohammad@gmail.com>
2024-07-28 16:14:33 -0500

    This's the initial commit for my personal portfolio for headstarter fellowship

---
+++ src/components/NavBar.js
@@ -0,0 +1,62 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect } from 'react';
+
+function NavBar() {
+    //activeLink is the state
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value)
+    }
+    
+    return (
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span class="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
+                        <Nav.Link href="#skils" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Link</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
+                    </Nav>
+                    <span className='navbar-text'>
+                        <div className='social-icon'>
+                            <a href='#'><img src={''} alt="" /></a>
+                            <a href='#'><img src={''} alt="" /></a>
+                            <a href='#'><img src={''} alt="" /></a>
+                        </div>
+                        <button className='vvd' onClick={() => { console.log('connect button pressed') }}>
+                            <span>
+                                Let's Connect
+                            </span>
+                        </button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
